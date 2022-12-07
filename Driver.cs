21334e0b70780c8f2a571d466a5bc30dea2beec2
Suryansh_72 <71132976+suryansh1972@users.noreply.github.com>
2022-12-07 21:36:12 +0530

    Add files via upload

---
+++ Driver.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pillars
+{
+    public class Driver
+    {
+        static void Main(string[] args)
+        {
+            List<Creator> creators = new List<Creator>() { new Youtuber(), new Programmer() };
+            foreach (var creator in creators)
+            {
+                creator.Create();
+                Console.WriteLine("Losers");
+            }
+        }
+    }
+}
